aef0fd08625a5e28c6c2467fa85c671ec0a8cedb
nkdudley <c_dudley@coloradocollege.edu>
2022-10-02 18:05:16 -0600

    more packet logic

--- src/packet.py
+++ src/packet.py
@@ -1,39 +1,42 @@
 class Packet:
 	def __init__(self, size, number, address, message):
-		self.size = intToBin(size,6)
-		self.number = intToBin(number,6)
-		self.address = intToBin(address,3)
-		self.message = charsToBin(message)
-	
+		self.size = size
+		self.number = number
+		self.address = address
+		self.message = message
+
 	def getBinString(self):
-		return self.message+self.address+self.number+self.size
-	
+		return (charsToBin(self.message)+
+			intToBin(self.address,3)+
+			intToBin(self.number,6)+
+			intToBin(self.size,6))
+
 	def getMessage(self):
-		return(binToChars(self.message))
+		return self.message
 
 def intToBin(num, length):
-	binNum = int(bin(num).replace("0b",""))
-	zeros = ""
-	for i in range(0,length):
-		zeros += '0'
-	zeros = int(zeros)
-	return zeros | binNum
+	b = bin(int(num)).replace("0b","")
+	for i in range(0,length-len(b)):
+		b = '0'+b
+	return b
 	
 def charsToBin(chars):
+	message=""
 	a1 = ord(chars[0])
-	print(a1)
 	a2 = ord(chars[1])
-	print(a2)
 	b1 = bin(a1).replace("0b","")
-	print(b1)
 	b2 = bin(a2).replace("0b","")
-	print(b2)
-	print(b1+b2)
-	return b1+b2	
+	for i in range(0,8-len(b1)):
+		message += '0'
+	message += b1
+	for i in range(0,8-len(b2)):
+		message += '0'
+	message += b2
+	return message
 
 def binToChars(binary):
-	input_value = int(binary)
-	input_bytes = input_value.to_bytes(4, "big")
+	input_value = int(binary,2)
+	input_bytes = input_value.to_bytes(2, "big")
 	ASCII_value = input_bytes.decode()
 	return ASCII_value
 
@@ -42,6 +45,9 @@ Currently the characters are being represented by 7 bits
 and must be represented by 4 bits before being passed to to_bytes()
 """
 
-test = Packet(1,1,1,"ab")
-print(test.message)
-print(test.getMessage())
+test = Packet(input("number of packets(1-63):"),
+	input("packet number(1-63):"),
+	input("address(0-7):"),
+	input("message(2 chars):"))
+print("binary string:",test.getBinString())
+print("message:",test.getMessage())
